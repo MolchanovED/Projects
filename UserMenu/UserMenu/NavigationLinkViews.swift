c08b0a43c2df8a34ee4e71afb5b4a1b0d6539642
MolchanovED <93006877+MolchanovED@users.noreply.github.com>
2021-11-22 19:14:41 +0200

    Tickets

---
+++ UserMenu/UserMenu/NavigationLinkViews.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkViews.swift
+//  UserMenu
+//
+//  Created by EgorM on 19.10.2021.
+//
+
+import SwiftUI
+
+
+struct AirTicketsList: View {
+    var body: some View{
+        NavigationView{
+            
+        }
+    }
+}
---
+++ UserMenu/UserMenu/UserMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserMenuApp.swift
+//  UserMenu
+//
+//  Created by EgorM on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct UserMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
